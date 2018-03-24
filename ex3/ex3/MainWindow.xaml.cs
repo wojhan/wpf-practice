8c98476c8f34bab3d2c3002e34beeb830da8f1a2
Wojtek <w.haniewski@gmail.com>
2018-03-24 16:06:39 +0100

    Create 5th app

---
+++ ex3/ex3/MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MarginSlider.Value = 5;
+            PaddingSlider.Value = 5;
+            BackgroundR.Value = BackgroundG.Value = BackgroundB.Value = 255;
+            ForegroundR.Text = ForegroundG.Text = ForegroundB.Text = "0";
+            ContentBox.Text = "Ala ma kota";
+            FontFamilyBox.Text = Result.FontFamily.ToString();
+            FontSizeBox.Text = Result.FontSize.ToString();
+            Brush.Text = "Black";
+            Thickness.Value = 1;
+        }
+
+        private void MarginSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Result.Margin = new Thickness(MarginSlider.Value);
+        }
+
+        private void PaddingSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Result.Padding = new Thickness(PaddingSlider.Value);
+        }
+
+        private void Background_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Result.Background = new SolidColorBrush(Color.FromRgb((byte)BackgroundR.Value, (byte)BackgroundG.Value, (byte)BackgroundB.Value));
+        }
+
+        private void Foreground_LostFocus(object sender, RoutedEventArgs e)
+        {
+            byte rColor, gColor, bColor;
+
+            byte.TryParse(ForegroundR.Text, out rColor);
+            byte.TryParse(ForegroundG.Text, out gColor);
+            byte.TryParse(ForegroundB.Text, out bColor);
+
+            Color color = Color.FromRgb(rColor, gColor, bColor);
+
+            Result.Foreground = new SolidColorBrush(color);
+        }
+
+        private void Brush_TextValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Result.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString(Brush.Text));
+            }
+            catch (FormatException)
+            {
+                Result.BorderBrush = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void Thickness_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Result.BorderThickness = new Thickness(Thickness.Value);
+        }
+
+        private void ItalicCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            if (ItalicCheckbox.IsChecked == true)
+            {
+                Result.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                Result.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void BoldCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            if (BoldCheckbox.IsChecked == true)
+            {
+                Result.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                Result.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void FontFamily_LostFocus(object sender, RoutedEventArgs e)
+        {
+            Result.FontFamily = new FontFamily(FontFamilyBox.Text);
+        }
+
+        private void FontSize_LostFocus(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Result.FontSize = Convert.ToDouble(FontSizeBox.Text);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message + "\nWpisz poprawną wartość dla rozmiaru czcionki.");
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message + "\nWpisz poprawną wartość dla rozmiaru czcionki.");
+            }
+
+        }
+
+        private void Content_TextChanged(object sender, RoutedEventArgs e)
+        {
+            Result.Content = ContentBox.Text;
+        }
+
+        private void HorizontalAlignment_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+
+            HorizontalAlignment alignment;
+
+            Enum.TryParse(radio.Content.ToString(), out alignment);
+
+            Result.HorizontalAlignment = alignment;
+        }
+
+        private void VerticalAlignment_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+
+            VerticalAlignment alignment;
+
+            Enum.TryParse(radio.Content.ToString(), out alignment);
+
+            Result.VerticalAlignment = alignment;
+        }
+
+        private void HorizontalContentAlignment_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+
+            HorizontalAlignment alignment;
+
+            Enum.TryParse(radio.Content.ToString(), out alignment);
+
+            Result.HorizontalContentAlignment = alignment;
+        }
+
+        private void VerticalContentAlignment_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+
+            VerticalAlignment alignment;
+
+            Enum.TryParse(radio.Content.ToString(), out alignment);
+
+            Result.VerticalContentAlignment = alignment;
+        }
+    }
+}
---
+++ ex4/ex4/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double lastX, lastY;
+        private Rectangle lastRectangle;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Rectangle CreateRectangle(double x, double y)
+        {
+            Random r = new Random();
+            Rectangle rectangle = new Rectangle();
+            canvas.Children.Add(rectangle);
+            rectangle.SetValue(Canvas.LeftProperty, x);
+            rectangle.SetValue(Canvas.TopProperty, y);
+            lastX = x;
+            lastY = y;
+            rectangle.Stroke = new SolidColorBrush(Color.FromRgb((byte)r.Next(0, 255), (byte)r.Next(0, 255), (byte)r.Next(0, 255)));
+            rectangle.StrokeThickness = 3;
+            return rectangle;
+        }
+
+        private void ResizeRectangle(Rectangle rectangle, double x, double y)
+        {
+            double startX, startY;
+
+            startX = lastX;
+            startY = lastY;
+
+            if (x - startX > 0)
+            {
+                rectangle.Width = x - startX;
+            }
+            else
+            {
+                rectangle.SetValue(Canvas.LeftProperty, x);
+                rectangle.Width = startX - x;
+            }
+
+            if (y - startY > 0)
+            {
+                rectangle.Height = y - startY;
+            }
+            else
+            {
+                rectangle.SetValue(Canvas.TopProperty, y);
+                rectangle.Height = startY - y;
+            }
+
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.Capture(canvas);
+            Point pt = e.GetPosition(canvas);
+            if (lastRectangle != null)
+                lastRectangle.StrokeThickness = 1;
+            lastRectangle = CreateRectangle(pt.X, pt.Y);
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.Capture(null);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            double move = 4;
+            double lastX, lastY;
+            lastX = (double)lastRectangle.GetValue(Canvas.LeftProperty);
+            lastY = (double)lastRectangle.GetValue(Canvas.TopProperty);
+            switch (e.Key)
+            {
+                case Key.Up:
+                    if (Keyboard.IsKeyDown(Key.LeftShift))
+                    {
+                        if (lastRectangle.Height - move > 0)
+                            lastRectangle.Height -= move;
+                    }
+                    else
+                    {
+                        if (lastY - move > 0)
+                        {
+                            lastRectangle.SetValue(Canvas.TopProperty, lastY - move);
+                            lastY -= move;
+                        }
+                    }
+                    break;
+                case Key.Down:
+                    if (Keyboard.IsKeyDown(Key.LeftShift))
+                    {
+                        lastRectangle.Height += move;
+                    }
+                    else
+                    {
+                        lastRectangle.SetValue(Canvas.TopProperty, lastY + move);
+                        lastY += move;
+                    }
+                    break;
+                case Key.Left:
+                    if (Keyboard.IsKeyDown(Key.LeftShift))
+                    {
+                        if (lastRectangle.Width - move >= 1)
+                            lastRectangle.Width -= move;
+                    }
+                    else
+                    {
+                        if (lastX - move >= 1)
+                        {
+                            lastRectangle.SetValue(Canvas.LeftProperty, lastX - move);
+                            lastX -= move;
+                        }
+                    }
+                    break;
+                case Key.Right:
+                    if (Keyboard.IsKeyDown(Key.LeftShift))
+                    {
+                        lastRectangle.Width += move;
+                    }
+                    else
+                    {
+                        lastRectangle.SetValue(Canvas.LeftProperty, lastX + move);
+                        lastX += move;
+                    }
+                    break;
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point pt = e.GetPosition(canvas);
+            if (e.LeftButton == MouseButtonState.Pressed && lastRectangle != null)
+            {
+                ResizeRectangle(lastRectangle, pt.X, pt.Y);
+            }
+        }
+    }
+}
